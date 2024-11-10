ad98dfb04784f576900c51e068a8726a1c379979
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2024-11-10 23:13:03 +0530

    Add files via upload

---
+++ Question3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Question3
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the length of the subarray");
+            int subArrayLength = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the size of the input array");
+            int sizeOfInput = int.Parse(Console.ReadLine());
+            int[] inputArray = new int[sizeOfInput];
+            Console.WriteLine("Enter the elements of the array");
+            for(int i=0;i<sizeOfInput;i++)
+            {
+                inputArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            //finding the maximum of the element in the sub array:
+            int minSubValue = int.MaxValue;
+            int maxElement=0;
+            for(int i=0;i<inputArray.Length-subArrayLength;i++)
+            {
+                maxElement = inputArray[i];
+                for(int j=i;j<i+subArrayLength;j++)
+                {
+                    if(inputArray[j]>maxElement)
+                    {
+                        maxElement = inputArray[j];
+                    }
+                }
+
+                if(maxElement < minSubValue)
+                {
+                    minSubValue = maxElement;
+                }
+            }
+
+            Console.WriteLine("Minimum Value is: "+minSubValue);
+        }
+    }
+}
